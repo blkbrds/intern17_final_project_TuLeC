d0827bce94c664570c1e93f708205ee7aaf29797
Tu Le C. [2] <tu.le2@monstar-lab.com>
2022-07-25 09:50:17 +0700

    Implement define API

---
+++ FinalProject/Models/Core/ApiManager.Video.swift
@@ -0,0 +1,90 @@
+//
+//  ApiManager.Video.swift
+//  FinalProject
+//
+//  Created by tu.le2 on 25/07/2022.
+//
+
+import Foundation
+
+extension ApiManager.Video {
+    struct QueryString {
+        func getUpComing() -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.moviePath +
+            ApiManager.Path.upComing +
+            ApiManager.Path.apiKey
+        }
+
+        func getTopRated() -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.moviePath +
+            ApiManager.Path.topRated +
+            ApiManager.Path.apiKey
+        }
+
+        func getPopular() -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.moviePath +
+            ApiManager.Path.popular +
+            ApiManager.Path.apiKey
+        }
+
+        func getNowPlaying() -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.moviePath +
+            ApiManager.Path.nowPlaying +
+            ApiManager.Path.apiKey
+        }
+
+        func getLatest() -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.moviePath +
+            ApiManager.Path.latest +
+            ApiManager.Path.apiKey
+        }
+
+        func getVideos(movieID: String) -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.moviePath +
+            "/\(movieID)/videos" +
+            ApiManager.Path.apiKey
+        }
+
+        func getDetails(movieID: String) -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.moviePath +
+            "/\(movieID)" +
+            ApiManager.Path.apiKey
+        }
+
+        func getRecommendations(movieID: String) -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.moviePath +
+            "/\(movieID)/recommendations" +
+            ApiManager.Path.apiKey
+        }
+
+        func searchMovies(query: String) -> String {
+            return ApiManager.Path.baseURL +
+            ApiManager.Path.version +
+            ApiManager.Path.searchPath +
+            ApiManager.Path.moviePath +
+            ApiManager.Path.apiKey +
+            "&query=\(query)"
+        }
+
+        func getImage(imagePath: String) -> String {
+            return ApiManager.Path.imageURL +
+            "/\(imagePath)"
+        }
+    }
+}
--- FinalProject/Models/Core/ApiManagerPath.swift
+++ FinalProject/Models/Core/ApiManagerPath.swift
@@ -10,10 +10,20 @@ import Foundation
 extension ApiManager {
 
     struct Path {
-        static let baseURL: String = "https://fxstudio.dev"
+        static let baseURL: String = "https://api.themoviedb.org"
+        static let imageURL: String = "https://image.tmdb.org/t/p/w500"
 
-        // example
+        static let version: String = "/3"
 
-        static let networkPath: String = baseURL + "/basic-ios-tutorial-connect-networking/"
+        static let moviePath: String = "/movie"
+        static let searchPath: String = "/search"
+
+        static let popular: String = "/popular"
+        static let upComing: String = "/upcoming"
+        static let topRated: String = "/top_rated"
+        static let nowPlaying: String = "/now_playing"
+        static let latest: String = "/latest"
+
+        static let apiKey: String = "?api_key=be80edab20fccfb19907902f8eccdd6b"
     }
 }
